c1ac68a74e22dc9e0a474aa1d68a91168e9182d5
Yu <lkmcfj@outlook.com>
2022-01-17 00:09:22 +0800

    fix order in __init__ and export sig_death

--- mcBasicLib/__init__.py
+++ mcBasicLib/__init__.py
@@ -12,6 +12,7 @@ class Export(QtCore.QObject):
     sig_input = QtCore.pyqtSignal(tuple)
     sig_login = QtCore.pyqtSignal(object)
     sig_logout = QtCore.pyqtSignal(object)
+    sig_death = QtCore.pyqtSignal(object)
     sig_advancement = QtCore.pyqtSignal(object)
     def __init__(self):
         super().__init__()
@@ -19,6 +20,9 @@ class Export(QtCore.QObject):
 
 def load(logger, core):
     # Function "load" is required by mana9er-core.
+    plugin_dir = os.path.join(core.root_dir, 'mcBasicLib')
+    AdvancementInfo.init(plugin_dir)
+    DeathMsgInfo.init(plugin_dir)
     lib_inst = McBasicLib(logger, core)
     exports = Export()
     exports.Player = Player
@@ -26,12 +30,10 @@ def load(logger, core):
     lib_inst.sig_input.connect(exports.sig_input)
     lib_inst.sig_login.connect(exports.sig_login)
     lib_inst.sig_logout.connect(exports.sig_logout)
+    lib_inst.sig_death.connect(exports.sig_death)
     lib_inst.sig_advancement.connect(exports.sig_advancement)
     exports.say = lib_inst.say
     exports.tellraw = lib_inst.tellraw
     exports.tell = lib_inst.tell
     exports.get_online_player_list = lib_inst.get_online_player_list
-    plugin_dir = os.path.join(core.root_dir, 'mcBasicLib')
-    AdvancementInfo.init(plugin_dir)
-    DeathMsgInfo.init(plugin_dir)
     return exports
