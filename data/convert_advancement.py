7ed4137bf6f6d53a2af0024f50fe66b6ddebd142
Asudy <asudy.w@gmail.com>
2025-05-13 02:01:31 +0800

    update 1.21.1 advancements

--- data/convert_advancement.py
+++ data/convert_advancement.py
@@ -1,9 +1,9 @@
 import csv
 
-with open('advancement_1.18.csv', 'r', encoding='utf-8', newline='') as f:
-    with open('../mcBasicLib/assets/advancement_ch.txt', 'w', encoding='utf-8') as ch_f:
+with open('advancement_1.21.1.csv', 'r', encoding='utf-8', newline='') as f:
+    with open('../mcBasicLib/assets/advancement_zh-cn.txt', 'w', encoding='utf-8') as ch_f:
         with open('../mcBasicLib/assets/advancement_en.txt', 'w', encoding='utf-8') as en_f:
             reader = csv.reader(f)
-            for row in list(reader):
+            for row in reader:
                 en_f.write(row[1] + '\n')
                 ch_f.write(row[2] + '\n')
