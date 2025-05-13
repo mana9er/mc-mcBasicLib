c251ba27d9e7e96c51da207c1cf1741ce9edb58a
Asudy <asudy.w@gmail.com>
2025-05-14 01:24:01 +0800

    update 1.21.1 death messages

--- data/convert_deathmsg.py
+++ data/convert_deathmsg.py
@@ -1,9 +1,9 @@
 import csv
 
-with open('deathmsg_1.18_rep.csv', 'r', encoding='utf-8', newline='') as f:
+with open('deathmsg_1.21.2_rep.csv', 'r', encoding='utf-8', newline='') as f:
     with open('../mcBasicLib/assets/deathmsg_zh-cn.txt', 'w', encoding='utf-8') as ch_f:
         with open('../mcBasicLib/assets/deathmsg_en.txt', 'w', encoding='utf-8') as en_f:
             reader = csv.reader(f)
             for row in list(reader):
-                en_f.write(row[1] + '\n')
-                ch_f.write(row[2] + '\n')
+                ch_f.write(row[1] + '\n')
+                en_f.write(row[2] + '\n')
